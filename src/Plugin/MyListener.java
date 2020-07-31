a387685ad75fe1c8bdfce6ab5f294aa8e20f3a28
Rapitharian <Rapitharian@hotmail.com>
2020-07-31 18:19:26 -0400

    Added Event and listerner

--- src/Plugin/MyListener.java
+++ src/Plugin/MyListener.java
@@ -2,7 +2,10 @@ package Plugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class MyListener implements Listener
@@ -10,6 +13,23 @@ public class MyListener implements Listener
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event)
     {
+        ExampleEvent exampleEvent = new ExampleEvent("Frontesque"); // Initialize your Event
+        Bukkit.getPluginManager().callEvent(exampleEvent); // This fires the event and allows any listener to listen to the event
+        if (!exampleEvent.isCancelled()) {
+            Bukkit.getPlayer("Frontesque").sendMessage(exampleEvent.getPlayerName()); // Use your event's data
+        }
         event.setJoinMessage("Welcome, " + event.getPlayer().getName() + " to the server!");
+    }
+    // Executes before the second method because it has a much lower priority.
+    @EventHandler (priority = EventPriority.LOWEST)
+    public void onPlayerChat1(AsyncPlayerChatEvent event)
+    {
+        event.setCancelled(true);
+    }
+    // Will not execute unless another listener with a  lower priority has uncancelled the event.
+    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerChat2(AsyncPlayerChatEvent event)
+    {
+        System.out.println("This shouldn't be executing.");
     }
 }
