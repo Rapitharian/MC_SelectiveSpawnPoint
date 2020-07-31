65f1e79ccca27c0cd45ca2ca17df6e60c6bcc52b
Rapitharian <Rapitharian@hotmail.com>
2020-07-31 16:52:24 -0400

    Create MyListener.java

---
+++ src/Plugin/MyListener.java
@@ -0,0 +1,15 @@
+package Plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class MyListener implements Listener
+{
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        event.setJoinMessage("Welcome, " + event.getPlayer().getName() + " to the server!");
+    }
+}
